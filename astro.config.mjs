602550c8be20548abdad230383cc429066212afd
Carlos Vibe <carlos@vibegaming.com>
2026-05-10 20:40:19 -0300

    Add Vercel adapter for SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import clerk from '@clerk/astro';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), clerk()],
   output: 'server',
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pt', 'es'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  adapter: vercel()
 });
